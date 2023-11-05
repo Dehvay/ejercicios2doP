2672530c8240b8f415077b071e155081f302ffdc
Dehvay <dymg.ngeva@outlook.com>
2023-11-04 23:01:20 -0600

    Programa que imprime una palabra dada por el usuario

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
-    
+    char Upalabra[20];
 
+    printf("Ingrese una palabra: ");
+    scanf_s("%s", Upalabra, 20);
 
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%s\n", Upalabra);
+    }
 
-
+    return 0;
 }
