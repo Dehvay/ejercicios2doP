c9d95aee7fc6a55d9486990a16c19231210a78ef
Dehvay <dymg.ngeva@outlook.com>
2023-11-06 21:20:51 -0600

    Programa juego del ahorcado

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -4,34 +4,54 @@
 
 int main()
 {
-    char frase[30];
-    int cont;
-    int let = 0;
+    char Palab[30];
+    char AstP[30];
+    int cont, cont2;
+    int vic = 5;
 
-    printf("Introduzca una frase: ");
-    gets_s(frase, 30);
+    char letA[5];
 
-    for (cont = 0; cont < frase[cont]; cont++)
+    printf("Introduzca la palabra a adivinar: ");
+    scanf_s("%s", Palab, 29);
+
+    for (cont = 0; cont < Palab[cont]; cont++)
     {
-        if (frase[cont] == frase[0])
+        AstP[cont] = '*';
+    }
+    printf("%s", AstP);
+
+    for (cont = 5; cont > 0; cont--)
+    {
+        printf("\nIntentos restantes %i\n", cont);
+        printf("Introduce una letra o palabra: ");
+        scanf_s("%s", letA, 29);
+
+        for (cont2 = 0; cont2 < Palab[cont2]; cont2++)
         {
-            frase[cont] = toupper(frase[cont]);
+            if (Palab[cont2] == letA[0])
+            {
+                AstP[cont2] = letA[0];
+            }
         }
-        else
+
+        if (strcmp(letA, Palab) == 0)
         {
-            frase[cont] = tolower(frase[cont]);
+            strcpy_s(AstP, 29, letA);
+            vic = 10;
+            cont = 1;
         }
+
+        printf("%s", AstP);
     }
 
-    if (frase[cont] == '.')
+    if (vic == 10)
     {
+        printf("\nFelicidades has adivinado la palabra\n");
     }
     else
     {
-        frase[cont] = '.';
+        printf("\nLastima, eres culpable de la muerte del ahorcado");
     }
-
-    printf("%s\n", frase);
 
     return 0;
 }
